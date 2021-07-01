77277aa088662b3b15d792b145f2305107ce88d4
闫兆祥 <73688938+yanzhaoxiang9812@users.noreply.github.com>
2021-07-01 11:28:00 +0800

    .

--- falsk_yiiqng/utils.py
+++ falsk_yiiqng/utils.py
@@ -14,8 +14,6 @@ from jieba.analyse import extract_tags
 def get_time():
     time_str = time.strftime("%Y{}%m{}%d{} %X")
     return time_str.format("年", "月", "日")
-
-
 # 连接数据库
 def get_conn():
     conn = pymysql.connect(
@@ -29,28 +27,17 @@ def get_conn():
     # 创建游标：
     cursor = conn.cursor()
     return conn, cursor
-
-
 def close_conn(conn, cursor):
     if cursor:
         cursor.close()
     if conn:
         conn.close()
-
-
 def query(sql, *args):
-    '''
-    :param sql:
-    :param args:
-    :return:返回结果，((),())形式
-    '''
     conn, cursor = get_conn()
     cursor.execute(sql, args)
     res = cursor.fetchall()  # 获取结果
     close_conn(conn, cursor)
     return res
-
-
 def get_c1_data():
     sql = "select sum(confirm)," \
           "(select suspect from history order by ds desc limit 1)," \
@@ -58,35 +45,23 @@ def get_c1_data():
           "sum(dead) " \
           "from details " \
           "where update_time=(select update_time from details order by update_time desc limit 1) "
-
     res = query(sql)
     return res[0]
-
-
 def get_c2_data():
     sql = "select province,sum(confirm) from details " \
           "where update_time=(select update_time from details " \
           "order by update_time desc limit 1) " \
           "group by province"
-
     res = query(sql)
     return res
-
-
 def get_l1_data():
     sql = "select ds,confirm,suspect,heal,dead from history"
-
     res = query(sql)
     return res
-
-
 def get_l2_data():
     sql = "select ds,confirm_add,suspect_add,heal_add,dead_add from history"
-
     res = query(sql)
     return res
-
-
 def get_r1_data():
     # union_all 两块相加
     sql = 'SELECT city,confirm FROM ' \
@@ -98,22 +73,11 @@ def get_r1_data():
           'where update_time=(select update_time from details order by update_time desc limit 1) ' \
           'and province in ("北京","上海","天津","重庆") group by province) as a ' \
           'ORDER BY confirm DESC limit 100'
-
     res = query(sql)
     return res
-
-
 def get_r2_data():
     sql = "select content from jinri_hot order by id"
-
     res = query(sql)
     return res
-
-
-from flask import jsonify
-
 if __name__ == '__main__':
-    # get_l1_data()
-    #  get_r1_data()
-    # get_r2_data()
     pass
